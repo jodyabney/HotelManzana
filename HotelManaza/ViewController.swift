36bbf8cd1b1c2f2191e0483499d44a79f12155ba
Jody Abney <jodyabney@gmail.com>
2020-04-28 08:40:30 -0400

    Initial Commit

---
+++ HotelManaza/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HotelManaza
+//
+//  Created by Jody Abney on 4/28/20.
+//  Copyright © 2020 AbneyAnalytics. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
